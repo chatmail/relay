1afdab7b205064ecfa8a43e46d02402c70cbf88a
missytake <missytake@systemli.org>
2025-07-08 16:44:28 +0200

    fix lint

--- chatmaild/src/chatmaild/doveauth.py
+++ chatmaild/src/chatmaild/doveauth.py
@@ -25,10 +25,15 @@ def is_allowed_to_create(config: Config, user, cleartext_password) -> bool:
     """Return True if user and password are admissable."""
     if os.path.exists(NOCREATE_FILE):
         if config.invite_token and config.invite_token not in cleartext_password:
-            logging.warning(f"blocked account creation because {NOCREATE_FILE!r} exists.")
+            logging.warning(
+                f"blocked account creation because {NOCREATE_FILE!r} exists."
+            )
             return False
 
-    if len(cleartext_password.replace(config.invite_token, "")) < config.password_min_length:
+    if (
+        len(cleartext_password.replace(config.invite_token, ""))
+        < config.password_min_length
+    ):
         logging.warning(
             "Password needs to be at least %s characters long",
             config.password_min_length,
