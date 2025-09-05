490776e74d98b36f181f513ad95755e5568876a9
Keonik1 <keonik.dev@gmail.com>
2025-09-05 20:39:00 +0300

    Add comment why variables are passed in setup_chatmail.service
    - https://github.com/chatmail/relay/pull/614#discussion_r2302077414

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -18,6 +18,9 @@ fi
 
 SETUP_CHATMAIL_SERVICE_PATH="${SETUP_CHATMAIL_SERVICE_PATH:-/lib/systemd/system/setup_chatmail.service}"
 
+# In systemd, you must EXPLICITLY specify which environment variables will be used by the process.
+# If they are not specified, the systemd unit will be started without variables.
+# This is a temporary solution until we completely move away from using systemd in the container.
 env_vars=$(printenv | cut -d= -f1 | xargs)
 sed -i "s|<envs_list>|$env_vars|g" $SETUP_CHATMAIL_SERVICE_PATH
 
