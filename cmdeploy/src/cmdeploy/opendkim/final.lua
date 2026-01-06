9ebcd498dbdb99b16c6c60a7627b8bc849994432
j4n <j4n@systemli.org>
2026-01-06 11:08:24 +0100

    fix: opendkim/final.lua: Delete up to 10 Authentication-Results headers
    There can be more than one header

--- cmdeploy/src/cmdeploy/opendkim/final.lua
+++ cmdeploy/src/cmdeploy/opendkim/final.lua
@@ -35,8 +35,10 @@ if valid then
 		odkim.del_header(ctx, "DKIM-Signature", i)
 	end
 
-	-- Delete first and presumably only occurence
-        odkim.del_header(ctx, "Authentication-Results", 0)
+	-- Delete up to 10 Authentication-Results headers
+	for i = 9, 0, -1 do
+		odkim.del_header(ctx, "Authentication-Results", 0)
+	end
 else
 	odkim.set_reply(ctx, "554", "5.7.1", error_msg)
 	odkim.set_result(ctx, SMFIS_REJECT)
