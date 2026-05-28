7bbf35796db45f7fa114703c2f570a916741dce7
𝔠𝔠𝔠𝔩𝔵𝔵𝔦𝔦𝔦 <151577046+ccclxxiii@users.noreply.github.com>
2026-05-28 13:01:25 +0000

    test: [wip] debug trace

--- cmdeploy/src/cmdeploy/tests/plugin.py
+++ cmdeploy/src/cmdeploy/tests/plugin.py
@@ -362,7 +362,8 @@ class ChatmailACFactory:
 
             # ensure messages stay in INBOX so that they can be
             # concurrently fetched via extra IMAP connections during tests
-            account.set_config("delete_server_after", "10")
+            # commented out to validate ci failure trace
+            # account.set_config("delete_server_after", "10")
             accounts.append(account)
 
         for future in futures:
